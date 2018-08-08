c7e404d13de34fa4123ae1b82e3567cc5c0c453a
Lucas Andrade <lap.andrade15@gmail.com>
2018-08-08 10:38:08 -0300

    Atualização

--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
@@ -1,7 +1,10 @@
 package br.ufrn.eaj.tads.testedaltonismo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botaoTeste1 = findViewById(R.id.botaoTeste1);
+        botaoTeste1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),TesteActivity.class);
+                intent.putExtra("valorImg", 1);
+
+                startActivityForResult(intent, 01);
+            }
+        });
+
+        Button botaoTeste2 = findViewById(R.id.botaoTeste2);
+        botaoTeste2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TesteActivity.class);
+                intent.putExtra("valorImg",02);
+                startActivityForResult(intent,01);
+            }
+        });
+
+        Button botaoTeste3 = findViewById(R.id.botaoTeste3);
+        botaoTeste3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),TesteActivity.class);
+                intent.putExtra("valorImg",03);
+                startActivityForResult(intent,01);
+            }
+        });
+
+        Button botaoVerificar = findViewById(R.id.botaoVerificar);
+        botaoVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Falta implementação
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //É o código padrão que foi passado
+        if(requestCode == 01){
+
+        }
     }
 }
--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
@@ -1,7 +1,14 @@
 package br.ufrn.eaj.tads.testedaltonismo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class TesteActivity extends AppCompatActivity {
 
@@ -9,5 +16,56 @@ public class TesteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teste);
+
+        Bundle params = getIntent().getExtras();
+        int valor = params.getInt("valorImg");
+        Log.i("valor","valor: "+valor);
+
+        //String caminho = "/res/drawable/teste"+valor;
+
+        ImageView iv = findViewById(R.id.imgTeste);
+        EditText texto = findViewById(R.id.campoValor);
+        String n = texto.getText().toString();
+        int n1 = Integer.parseInt(n);
+
+        //iv.setImage
+
+        if(valor == 1){
+            iv.setImageResource(R.drawable.teste1);
+
+            Intent intent = new Intent();
+            Bundle data = new Bundle();
+
+            data.putInt("img1",n1);
+            intent.putExtras(data);
+            setResult(15);
+        }else if(valor == 2){
+            iv.setImageResource(R.drawable.teste2);
+
+            Intent intent = new Intent();
+            Bundle data = new Bundle();
+
+            data.putInt("img2",n1);
+            intent.putExtras(data);
+            setResult(16);
+        }else if(valor == 3){
+            iv.setImageResource(R.drawable.teste3);
+
+            Intent intent = new Intent();
+            Bundle data = new Bundle();
+
+            data.putInt("img2",n1);
+            intent.putExtras(data);
+            setResult(17);
+        }
+
+        Button botaoOk = findViewById(R.id.botaoOk);
+        botaoOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
     }
 }
